70783ea12fa537b1d63c20febf287a5a3be96beb
PiyuCodere <piyushrai284@gmail.comm>
2024-01-20 02:04:49 +0530

    project description and resume update

--- src/dummyData/projectData.js
+++ src/dummyData/projectData.js
@@ -6,7 +6,7 @@ export const projectData = [
   {
     name: "KidQuest",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "An interactive learning platform for the kids where they can play and learn. It has two sections i.e. Kids and Parent. Kid section has multiple sections like Alphabets, Numbers, Colors, Animals, Slate, and Games which has sounds and a friendly UI along with some interesting quizzes. Parents can monitor and view the progress of the kid in their section.",
     image: kidQuestImg,
     github: "https://github.com/PiyuCoder/kid-quest.git",
     demo: "https://kidquest.onrender.com/",
@@ -15,7 +15,7 @@ export const projectData = [
   {
     name: "Resume Crafter",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Resume Crafter is an innovative application designed to streamline the resume-building process, offering users an intuitive and personalized experience for crafting impactful resumes effortlessly",
     image: resumeCrafter,
     github: "https://github.com/PiyuCoder/resume-builder.git",
     demo: "https://resume-crafter-6tub.onrender.com/",
@@ -24,7 +24,7 @@ export const projectData = [
   {
     name: "To-Do",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Empower your productivity with our advanced to-do app, delivering feature-rich task management, intuitive collaboration tools, and seamless integration to elevate your daily workflow and enhance your productivity experience",
     image: toDo,
     github: "https://github.com/PiyuCoder/to-do.git",
     demo: "https://todo-guhi.onrender.com/",
